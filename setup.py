559e59d535b53ef9c1887af1b48ffc51f63709f6
saikumar <sai680513@gmail.com>
2024-01-07 08:16:45 +0530

    Setup the project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PredictionEndpoint",
+    version="0.0.1",
+    author="Sai Kumar",
+    author_email="sai680513@gmail.com",
+    packages = find_packages(),
+)
